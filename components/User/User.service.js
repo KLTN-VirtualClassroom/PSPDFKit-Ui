4277c09de00df4c4866048a501ea98ac8ee4a4cd
Nghia-Hieu <63239928+Nghia-Hieu@users.noreply.github.com>
2023-03-07 09:57:21 +0700

    revert PSPDFKit UI

--- components/User/User.service.js
+++
@@ -1,9 +0,0 @@
-import UserModel from "../model/User.model.js";
-
-export async function getListUser(){
-    try {
-        return await UserModel.find();
-      } catch (error) {
-        throw new APIError(500, error.message);
-      }
-}
--- utils/socketLink.js
+++
@@ -1,5 +0,0 @@
-export const socketData = [
-    "http://localhost:4200",
-    "http://localhost:3303",
-    "http://115.78.232.219:3122",
-  ];
